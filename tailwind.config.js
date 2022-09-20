5e2148af395bcf0e72d289cb5694b911c3ffeff3
asadkhan-cc <asad.khan@cooperativecomputing.com>
2022-09-20 18:44:46 +0500

    Initial Proj Setup added Dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./Utils/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
