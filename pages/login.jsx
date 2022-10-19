48a5fb04841f6b64482f9d0ca3675c40a620fc61
asadkhan-cc <asad.khan@cooperativecomputing.com>
2022-10-19 16:17:47 +0500

    bug fix on login Page

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,10 +1,10 @@
+import { Button } from "antd";
 import { onAuthStateChanged } from "firebase/auth";
 import Head from "next/head";
 import Router, { useRouter } from "next/router";
 import { useState } from "react";
 import { auth } from "../FirebaseApp/firebase-config";
 import LoginPage from "../src/Pages/LogInPage";
-
 const Login = () => {
   // router = useRouter();
   const [user, setUser] = useState(null);
@@ -14,7 +14,13 @@ const Login = () => {
       <Head>
         <title>Login</title>
       </Head>
-      {user ? <div>Already LoggedIn</div> : <LoginPage />}
+      {user ? (
+        <div className="flex justify-center">
+          <Button loading={true}>Loading</Button>
+        </div>
+      ) : (
+        <LoginPage />
+      )}
     </>
   );
 };
